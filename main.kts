4c40c793a430ed01ce7789d5ded42ee5fc2c8b2d
Henry Zhang <henry.zhang@live.com>
2018-04-04 02:20:38 -0700

    Add whenFn function

--- main.kts
+++ main.kts
@@ -3,6 +3,16 @@
 println("UW Homework: Simple Kotlin")
 
 // write a "whenFn" that takes an arg of type "Any" and returns a String
+fun whenFn (arg : Any): String {
+    return when (arg) {
+        "Hello" -> "world"
+        "Howdy", "Bonjour" -> "Say what?" // why does the comma but || doesn't?
+        0 -> "zero"
+        1 -> "one"
+        in 2..10 -> "low number"
+        else -> "I don't understand"
+    }
+}
 
 // write an "add" function that takes two Ints, returns an Int, and adds the values
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
@@ -30,69 +40,69 @@ for ((k,v) in when_tests) {
 }
 println("")
 
-print("Add tests: ")
-val add_tests = listOf(
-    Pair(0, 0) to 0,
-    Pair(1, 2) to 3,
-    Pair(-2, 2) to 0,
-    Pair(123, 456) to 579
-)
-for ( (k,v) in add_tests) {
-    print(if (add(k.first, k.second) == v) "." else "!")
-}
-println("")
+// print("Add tests: ")
+// val add_tests = listOf(
+//     Pair(0, 0) to 0,
+//     Pair(1, 2) to 3,
+//     Pair(-2, 2) to 0,
+//     Pair(123, 456) to 579
+// )
+// for ( (k,v) in add_tests) {
+//     print(if (add(k.first, k.second) == v) "." else "!")
+// }
+// println("")
 
-print("Sub tests: ")
-val sub_tests = listOf(
-    Pair(0, 0) to 0,
-    Pair(2, 1) to 1,
-    Pair(-2, 2) to -4,
-    Pair(456, 123) to 333
-)
-for ( (k,v) in sub_tests) {
-    print(if (sub(k.first, k.second) == v) "." else "!")
-}
-println("")
+// print("Sub tests: ")
+// val sub_tests = listOf(
+//     Pair(0, 0) to 0,
+//     Pair(2, 1) to 1,
+//     Pair(-2, 2) to -4,
+//     Pair(456, 123) to 333
+// )
+// for ( (k,v) in sub_tests) {
+//     print(if (sub(k.first, k.second) == v) "." else "!")
+// }
+// println("")
 
-print("Op tests: ")
-print(if (mathOp(2, 2, { l,r -> l+r} ) == 4) "." else "!")
-print(if (mathOp(2, 2, ::add ) == 4) "." else "!")
-print(if (mathOp(2, 2, ::sub ) == 0) "." else "!")
-print(if (mathOp(2, 2, { l,r -> l*r} ) == 4) "." else "!")
-println("")
+// print("Op tests: ")
+// print(if (mathOp(2, 2, { l,r -> l+r} ) == 4) "." else "!")
+// print(if (mathOp(2, 2, ::add ) == 4) "." else "!")
+// print(if (mathOp(2, 2, ::sub ) == 0) "." else "!")
+// print(if (mathOp(2, 2, { l,r -> l*r} ) == 4) "." else "!")
+// println("")
 
 
-print("Person tests: ")
-val p1 = Person("Ted", "Neward", 47)
-print(if (p1.firstName == "Ted") "." else "!")
-p1.age = 48
-print(if (p1.debugString == "[Person firstName:Ted lastName:Neward age:48]") "." else "!")
-println("")
+// print("Person tests: ")
+// val p1 = Person("Ted", "Neward", 47)
+// print(if (p1.firstName == "Ted") "." else "!")
+// p1.age = 48
+// print(if (p1.debugString == "[Person firstName:Ted lastName:Neward age:48]") "." else "!")
+// println("")
 
-print("Money tests: ")
-val tenUSD = Money(10, "USD")
-val twelveUSD = Money(12, "USD")
-val fiveGBP = Money(5, "GBP")
-val fifteenEUR = Money(15, "EUR")
-val fifteenCAN = Money(15, "CAN")
-val convert_tests = listOf(
-    Pair(tenUSD, tenUSD),
-    Pair(tenUSD, fiveGBP),
-    Pair(tenUSD, fifteenEUR),
-    Pair(twelveUSD, fifteenCAN),
-    Pair(fiveGBP, tenUSD),
-    Pair(fiveGBP, fifteenEUR)
-)
-for ( (from,to) in convert_tests) {
-    print(if (from.convert(to.currency).amount == to.amount) "." else "!")
-}
-val moneyadd_tests = listOf(
-    Pair(tenUSD, tenUSD) to Money(20, "USD"),
-    Pair(tenUSD, fiveGBP) to Money(20, "USD"),
-    Pair(fiveGBP, tenUSD) to Money(10, "GBP")
-)
-for ( (pair, result) in moneyadd_tests) {
-    print(if ((pair.first + pair.second).amount == result.amount &&
-              (pair.first + pair.second).currency == result.currency) "." else "!")
-}
-println("")
+// print("Money tests: ")
+// val tenUSD = Money(10, "USD")
+// val twelveUSD = Money(12, "USD")
+// val fiveGBP = Money(5, "GBP")
+// val fifteenEUR = Money(15, "EUR")
+// val fifteenCAN = Money(15, "CAN")
+// val convert_tests = listOf(
+//     Pair(tenUSD, tenUSD),
+//     Pair(tenUSD, fiveGBP),
+//     Pair(tenUSD, fifteenEUR),
+//     Pair(twelveUSD, fifteenCAN),
+//     Pair(fiveGBP, tenUSD),
+//     Pair(fiveGBP, fifteenEUR)
+// )
+// for ( (from,to) in convert_tests) {
+//     print(if (from.convert(to.currency).amount == to.amount) "." else "!")
+// }
+// val moneyadd_tests = listOf(
+//     Pair(tenUSD, tenUSD) to Money(20, "USD"),
+//     Pair(tenUSD, fiveGBP) to Money(20, "USD"),
+//     Pair(fiveGBP, tenUSD) to Money(10, "GBP")
+// )
+// for ( (pair, result) in moneyadd_tests) {
+//     print(if ((pair.first + pair.second).amount == result.amount &&
+//               (pair.first + pair.second).currency == result.currency) "." else "!")
+// }
+// println("")
